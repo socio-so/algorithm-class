c443853772d19d627dd0dad8c47cce96396f48c5
socio-so <wlghks0227@gmail.com>
2025-09-09 17:52:26 +0900

    Add files via upload
    스텍 클래스 기반 문자열 역순 출력 코드 추가

--- stack_class.py
+++ stack_class.py
@@ -1,1 +1,71 @@
 # define stack class with push, pop, peek, is_empty, and size methods
+# Stack ADT
+
+
+class ArrayStack:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.array = [None]*self.capacity
+        self.top = -1
+        
+        
+    def is_empty(self):
+        return self.top == -1
+        
+
+    def is_full(self):
+        return self.top == self.capacity-1
+    
+
+    def push(self, item):
+        if not self.is_full():
+            self.top += 1
+            self.array[self.top] = item
+            print("PUSH : {item!r} -> stack is now {self.array[:self.top + 1]}")
+        else :
+            raise OverflowError("Stack Overflow")
+
+
+    def pop(self):
+        if not self.is_empty():
+            item = self.array[self.top]
+            self.array[self.top] = None
+            self.top -= 1
+            print(f"POP : {item!r} -> stack is now {self.array[:self.top + 1]}")
+            return item
+        else:
+            raise IndexError("Stack underflower")
+        
+    
+    def peek(self):
+        if not self.is_empty():
+            return self.array[self.top]
+        return None
+    
+
+    def size(self):
+        return self.top + 1
+    
+
+#Test the Stack class
+
+
+def reverse_string(statement):
+    print("\n[1] PUSH 단계 -----------------------------")
+    st = ArrayStack(len(statement))
+    for char in statement:
+        st.push(char)
+
+    print("\n[2] POP 단계 -----------------------------")
+    out = []
+    while not st.is_empty():
+        out.append(st.pop())
+
+    result = ''.join(out)
+    print("\n[3] 최종 단계 : {result}")
+    return result
+
+
+if __name__ == "__main__":
+    statement = "안녕하세요, 반갑습니다."
+    reverse_string(statement)
